c3bc72967f589c253a5a898b68b39c0d1c233fc5
Marcos Antônio <109171686+marcosantonioh@users.noreply.github.com>
2024-07-27 16:53:31 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -115,3 +115,18 @@ function redirectToConfirmation(event) {
 
 
 
+
+
+
+let menuList = document.getElementById("nav-links")
+menuList.style.maxHeight = "0px";
+
+function toggleMenu(){
+    if(menuList.style.maxHeight == "0px")
+    {
+        menuList.style.maxHeight = "300px";
+    }
+    else{
+        menuList.style.maxHeight = "0px";
+    }
+}
